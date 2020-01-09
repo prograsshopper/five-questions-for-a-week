5c1dde9712399d582e769c90012d4f974284a0ff
prograsshopper <tjalwled12@gmail.com>
2020-01-09 17:57:48 +0900

    week 1 : Subarray - nonpass

---
+++ week_1/subarray_sum.py
@@ -0,0 +1,20 @@
+# 1 Brute Force : Time Exceed
+def subarraySum(nums):
+        if len(nums) == 0:
+            return 0
+        if len(nums) <= 1:
+            if sum(nums) == k:
+                return 1
+            return 0
+        result = 0
+        for i in range(0, len(nums)-1):
+            current_sum = nums[i]
+            if current_sum == k:
+                result+=1
+            for j in range(i+1, len(nums)):
+                current_sum += nums[j]
+                if current_sum == k:
+                    result += 1
+            if i == len(nums) -2 and nums[i+1] == k:
+                result += 1
+        return result
