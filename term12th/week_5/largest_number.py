ca9d02eac4d4f6798543103d4d3aaf70b0067cee
prograsshopper <tjalwled12@gmail.com>
2020-08-14 18:20:43 +0900

    week 5: Largest Number

---
+++ term12th/week_5/largest_number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i in range(len(nums), 0, -1):
+            for j in range(0, i-1):
+                if not self.compare(nums[j], nums[j+1]):
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+        return str(int("".join([str(num) for num in nums])))
+
+    def compare(self, h, t):
+        return str(h)+str(t) > str(t)+str(h)
