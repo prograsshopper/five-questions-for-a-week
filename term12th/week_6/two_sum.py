0b0ae8f8b57d204727923730ffd66f44e4890f54
prograsshopper <tjalwled12@gmail.com>
2020-08-21 18:48:36 +0900

    week 6: Two Sum update

--- term12th/week_6/two_sum.py
+++ term12th/week_6/two_sum.py
@@ -1,7 +1,17 @@
 class Solution:
+    # old version
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(0, len(nums)-1):
             remain = target - nums[i]
             for j in range(i+1, len(nums)):
                 if remain == nums[j]:
                     return [i, j]
+    
+    # new - use hash table
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}
+        for i in range(len(nums)):
+            if nums[i] not in num_dict:
+                num_dict[target - nums[i]] = i
+            else:
+                return num_dict[nums[i]], i
